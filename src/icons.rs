16becd36aff814cd0f91cb7c99219ee9b5c51de3
Antonio Pitasi <antonio@pitasi.dev>
2023-07-21 19:29:45 +0200

    complete homepage

--- src/icons.rs
+++ src/icons.rs
@@ -26,3 +26,21 @@ pub fn logout() -> Markup {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
    "#.into())
 }
+
+pub fn twitter() -> Markup {
+    PreEscaped(r#"
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg>
+   "#.into())
+}
+
+pub fn github() -> Markup {
+    PreEscaped(r#"
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
+   "#.into())
+}
+
+pub fn linkedin() -> Markup {
+    PreEscaped(r#"
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+   "#.into())
+}
--- src/main.rs
+++ src/main.rs
@@ -389,6 +389,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     #[cfg(debug_assertions)]
     let router = router.layer(tower_livereload::LiveReloadLayer::new().request_predicate(not_htmx));
+    #[cfg(debug_assertions)]
+    println!("Live reload enabled");
 
     let addr: SocketAddr = (
         IpAddr::from_str(&env::var("HOST").unwrap_or("127.0.0.1".into()))?,
@@ -555,40 +557,122 @@ fn root(uri: &http::Uri, meta: Meta, slot: Markup, user: Option<User>) -> Markup
 }
 
 async fn page_home(uri: http::Uri, Extension(auth): Extension<AuthContext>) -> Markup {
+    let socials = [
+        ("Twitter", icons::twitter(), "https://twitter.com/zaphodias"),
+        ("Github", icons::github(), "https://github.com/pitasi"),
+        (
+            "LinkedIn",
+            icons::linkedin(),
+            "https://www.linkedin.com/in/pitasi/",
+        ),
+    ];
+
+    let work_experiences = [
+        (
+            "Qredo",
+            "Blockchain Engineer",
+            "/static/companies/qredo.png",
+            "2022",
+            "Present",
+        ),
+        (
+            "Ignite (fka Tendermint)",
+            "Sr. Backend Engineer",
+            "/static/companies/tendermint.svg",
+            "2022",
+            "2022",
+        ),
+        (
+            "Geckosoft",
+            "Backend Engineer",
+            "/static/companies/geckosoft.svg",
+            "2020",
+            "2022",
+        ),
+        (
+            "Nextworks",
+            "Backend Engineer",
+            "/static/companies/nextworks.svg",
+            "2019",
+            "2020",
+        ),
+        (
+            "Zerynth",
+            "Fullstack developer",
+            "/static/companies/zerynth.svg",
+            "2018",
+            "2019",
+        ),
+    ];
+
     root(
         &uri,
         Meta::default(),
         html! {
-                main class="typography mx-auto my-20 max-w-2xl space-y-16 px-6 text-liver lg:px-14" {
-                    section {
-            p {"
+            main class="mx-auto my-20 max-w-2xl space-y-16 px-6 text-liver lg:px-14" {
+                section class="typography" {
+                    p {"
 I'm Antonio, a backend software engineer. I'm passionate about distributed
 systems and clean maintainable software. In my free time, I organize events
-with the local community I founded: pisa.dev.
-"}
+with the local community I founded: " a href="https://pisa.dev" { "pisa.dev" } ". "}
 
-            p {"
-I'm currently working on exciting technology at Qredo. We aim to decentralize
-the private keys for your cryptocurrencies using our dMPC solution.
-"}
+                    p {"
+I'm currently working on exciting technology at " a href="https://qredo.com" { "Qredo" } ". We aim to decentralize
+the private keys for your cryptocurrencies using our dMPC solution. "}
 
-            p {"
-Before that, I worked at Ignite (also known as Tendermint), the company that
-first created Proof-of-Stake and Cosmos SDK. My role was Senior Backend
-Engineer for the (now defunct) Emeris.
-"}
+                    p {"
+Before that, I worked at " a href="https://ignite.com" { "Ignite" } " (also known as " a href="https://tendermint.com" { "Tendermint" } "), the company that
+first created " a href="https://blog.cosmos.network/cosmos-history-inception-to-prelaunch-b05bcb6a4b2b" { "Proof-of-Stake" } " and " a href="https://cosmos.network/" { "Cosmos SDK" } ". My role was Senior Backend
+Engineer for the " em { "(now defunct)" } " " a href="https://emeris.com" { "Emeris" } ". "}
 
-            p {"
+                    p {"
 Before diving into cryptocurrencies tech, I've cutted my teeth in fast-paced
-startups where I helped shaping products such as Traent and Zerynth.
-"}
+startups where I helped shaping products such as " a href="https://traent.com" { "Traent" } " and " a href="Zerynth" { "Zerynth" } ". "}
 
-            p {"
-Sometimes I have over-engineering tendencies, such as my personal website.
-Most of the times I'm harmless though.
-"}
-            }
-            a href="/articles" { "Read my articles" }
+                    p {"
+Sometimes I have over-engineering tendencies, such as " a href="https://github.com/Pitasi/univrs" { "my personal website" } ".
+Most of the times I'm harmless though. "}
+                }
+
+                section {
+                    ul class="flex flex-row gap-4" {
+                        @for (_, icon, href) in socials.iter() {
+                            li {
+                                a class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset0 disabled:opacity-50 disabled:pointer-events-none data-[state=open]:bg-slate-100 h-10 py-2 px-4"
+                                    href=(href) {
+                                    (icon)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                section {
+                    ol class="mt-6 space-y-6" {
+                        @for (name, title, src, from, to) in work_experiences.iter() {
+                            li class="flex gap-4" {
+                                img alt=(name) loading="lazy" decoding="async" width="48" height="48" class="h-7 w-7 rounded-full" src=(src);
+
+                                dl class="flex flex-auto flex-wrap gap-x-2" {
+                                    dt class="sr-only" { "Company" }
+                                    dd class="w-full flex-none text-sm font-medium text-black" { (name) }
+
+                                    dt class="sr-only" { "Role" }
+                                    dd class="text-xs text-eerie" { (title) }
+
+                                    dt class="sr-only" { "Date" }
+                                    dd class="ml-auto text-xs text-liver" aria-label=(format!("From {} to {}", from, to)) {
+                                        time datetime="2022" { (from) }
+                                        " "
+                                        span aria-hidden="true" { "—" }
+                                        " "
+                                        time datetime="Present" { (to) }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         },
         auth.current_user,
